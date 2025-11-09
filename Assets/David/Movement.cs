b7c22a9d26f08ac3c5971084707cf180ea27ad4b
David Olu-Talabi <davidolutalabi@gmail.com>
2025-11-09 13:10:09 -0500

    Added Jump Functionality
    Added jump functionality

--- Assets/David/Movement.cs
+++ Assets/David/Movement.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-     public float moveSpeed = 5f; // Adjust this value to control movement speed
+    public float moveSpeed = 5f;
+    public float jumpHeight = 10f; // Adjust this value to control movement speed
 
     // Update is called once per frame
     void Update()
@@ -20,5 +21,19 @@ public class Movement : MonoBehaviour
             // Move the object to the left along the X-axis
             transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
         }
-       }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            // Move the object forward along the Y-axis
+            transform.Translate(Vector3.up * jumpHeight * Time.deltaTime);
+        }
+    }
+
+    void Rotate()
+    {
+        // if (this.transform.rotation)
+        // {
+            
+        // }
+    }
 }

96ea2d8644928e09516a6d8da48a4afa4d88546c
Rimour <treycobb1@gmail.com>
2025-11-16 07:49:37 -0600

    Updated Main Menu
    Added Settings menu
    updated Main menu

---
+++ Assets/Dev-Trent/Scripts/SettingsManager.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    [Header("Settings Panel")]
+    public bool openSettings;
+    public GameObject SettingsPanel;
+    [Header("Audio")]
+    public AudioMixer AudioManager;
+    [Header("Graphic")]
+    public TMP_Dropdown GraphicDropdown;
+
+    public void Start()
+    {
+        GraphicDropdown.value = PlayerPrefs.GetInt("GraphicsSettings", 1);
+    }
+    public void ToggleSettings()
+    {
+        if (openSettings) { SettingsPanel.SetActive(false); openSettings = false; }else { SettingsPanel.SetActive(true); openSettings = true; }
+    }
+
+    public void SetMaterVolume(float volumeSlider) 
+    { 
+        AudioManager.SetFloat("MasterVol",Mathf.Log10(volumeSlider) * 20);
+    }
+
+    public void FullscreenControls()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void ChangeGraphics(int graphicIndex)
+    {
+        QualitySettings.SetQualityLevel(graphicIndex);
+        PlayerPrefs.SetInt("GraphicSettings", graphicIndex);
+    }
+}
